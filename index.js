a6cf90b9c2adf4f3782449bc13b639ed2628ee11
Seth <58237855+ThySeth@users.noreply.github.com>
2022-02-18 13:42:39 -0600

    send & receive inputs

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, ipcMain, ipcRenderer} = require("electron");
+const Wasteful = require("wastefuldb");
+const db = new Wasteful({serial: true, feedback: true, path: `${__dirname}/storage/`});
 
 async function winCreate() {
     let window = new BrowserWindow({
         width: 300,
         height: 400,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        autoHideMenuBar: true
     })
     
     window.loadFile("main.html");
+    window.webContents.on('did-finish-load', () => {
+      let data;
+        for(let i = 0; i < db.size(); i++) {
+            data = db.find({id: `${i.toString()}`}); 
+             data = data.length == 0 ? "<Unable to find note>" : data;
+              window.webContents.send("note:startup", data.note);
+        }
+    })
 }
 
+ipcMain.on("note:new", function(e, text) {
+    if(text.length == 0) return;
+     db.insert({note: text});
+})
 
-app.whenReady().then(() => {
+app.whenReady().then(async() => {
     if(BrowserWindow.getAllWindows().length === 0) {
         winCreate();
     }
